29d4d15dc8ff548a0f9e5f42e691813125136cbf
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-03-19 23:54:47 +0600

    cerate api for last_order, restaurant, customer, order_details

--- foodtaskapp/api/urls.py
+++ foodtaskapp/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
-from .views import customer_get_restaurant
+from . import views
 
 urlpatterns = [
-  path('restaurants/', customer_get_restaurant),
+
+  path('restaurants/', views.customer_get_restaurants),
+  path('meals/<restaurant_id>/', views.customer_get_meals),
+  path('order/add/', views.customer_add_order),
+  path('order/latest/', views.customer_get_latest_order),
+  path('order/notification/<last_request_time>/', views.restaurant_order_notification),
+
 ]
