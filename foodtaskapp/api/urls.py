0e3c18640c147cc22a0485a39584c4f12fbff8be
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-03-21 00:53:46 +0600

    diver location api

--- foodtaskapp/api/urls.py
+++ foodtaskapp/api/urls.py
@@ -3,19 +3,22 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+  # APIs for Customers
   path('customer/restaurants/', views.customer_get_restaurants),
   path('customer/meals/<restaurant_id>/', views.customer_get_meals),
   path('customer/order/add/', views.customer_add_order),
   path('customer/order/latest/', views.customer_get_latest_order),
+  path('api/customer/driver/location/', views.customer_driver_location),
 
   path('restaurant/order/notification/<last_request_time>/', views.restaurant_order_notification),
 
   # APIs for DRIVERS
-  path(r'^api/driver/orders/ready/', views.driver_get_ready_orders),
-  path(r'^api/driver/order/pick/', views.driver_pick_order),
-  path(r'^api/driver/order/latest/', views.driver_get_latest_order),
-  path(r'^api/driver/order/complete/', views.driver_complete_order),
-  path(r'^api/driver/revenue/', views.driver_get_revenue),
+  path('api/driver/orders/ready/', views.driver_get_ready_orders),
+  path('api/driver/order/pick/', views.driver_pick_order),
+  path('api/driver/order/latest/', views.driver_get_latest_order),
+  path('api/driver/order/complete/', views.driver_complete_order),
+  path('api/driver/revenue/', views.driver_get_revenue),
+  path('api/driver/location/update/', views.driver_update_location),
+
 
 ]
--- foodtaskapp/api/views.py
+++ foodtaskapp/api/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from oauth2_provider.models import AccessToken
 
-from foodtaskapp.models import Restaurant, Meal, Order, OrderDetails
+from foodtaskapp.models import Restaurant, Meal, Order, OrderDetails, Driver
 from .serializers import RestaurantSerializer, MealSerializer, OrderSerializer
 
 
@@ -96,6 +96,20 @@ def customer_get_latest_order(request):
     return JsonResponse({"order": order})
 
 
+def customer_driver_location(request):
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        expires__gt = timezone.now())
+
+    customer = access_token.user.customer
+
+    # Get driver's location related to this customer's current order.
+    current_order = Order.objects.filter(customer = customer, status = Order.DELIVERED).last()
+    location = current_order.driver.location
+
+    return JsonResponse({"location": location})
+
+
+###
 def restaurant_order_notification(request, last_request_time):
     notification = Order.objects.filter(restaurant = request.user.restaurant,
         created_at__gt = last_request_time).count()
@@ -206,6 +220,24 @@ def driver_get_revenue(request):
         revenue[day.strftime("%a")] = sum(order.total for order in orders)
 
     return JsonResponse({"revenue": revenue})
+
+
+
+# POST - params: access_token, "lat,lng"
+@csrf_exempt
+def driver_update_location(request):
+    if request.method == "POST":
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt = timezone.now())
+
+        driver = access_token.user.driver
+
+        # Set location string => database
+        driver.location = request.POST["location"]
+        driver.save()
+
+        return JsonResponse({"status": "success"})
+   
 
 
  
